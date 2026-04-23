74b87c7b8eb289639d0f85fae5740fa2269b30c6
Michael115555 <michaelvogel@hotmail.ch>
2026-04-23 15:28:38 +0200

    Clean up frontend template files

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,9 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -11,13 +8,7 @@ export default defineConfig({
     optimizeDeps: {
         noDiscovery: true
     },
-    plugins: [
-        vue(),
-        tailwindcss(),
-        Components({
-            resolvers: [PrimeVueResolver()]
-        })
-    ],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
